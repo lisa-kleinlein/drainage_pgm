2b18b9598be4aeff1e2fb718f4945f9d70cab862
lisa-kleinlein <lisakleinlein@campus.lmu.de>
2023-09-25 20:24:41 +0200

    Some maybe useful model diagnosis

--- model_data.R
+++ model_data.R
@@ -1,13 +1,20 @@
+#drainage_X10302 = Muenchen
+#drainage_X10303 = BadToelz
+#drainage_X10304 = Mittenwald
+#drainage_X10321 = Schlehdorf
+
+
 setwd("C:/Users/lisak/OneDrive/Dokumente/Studium/7. Semester/Bachelorarbeit/drainage_pgm/kbe")
 
 # load packages
 library(bnlearn)
 library(Rgraphviz)
+library(ggplot2)
 
 # load data
 data_merged_rol <- readRDS("data_merged_rol.rds")
 
-# bayes network
+# Bayesian network
 variables_needed <- c("drainage_X10302", "drainage_X10303", "drainage_X10304", "drainage_X10321",
                       "rol_airtmp_X10302", "rol_airtmp_X10303", "rol_airtmp_X10304", "rol_airtmp_X10321",
                       "rol_glorad_X10302", "rol_glorad_X10303", "rol_glorad_X10304", "rol_glorad_X10321",
@@ -61,8 +68,74 @@ network <- set.arc(network, "drainage_X10304", "drainage_X10303")
 network <- set.arc(network, "drainage_X10303", "drainage_X10302")
 network <- set.arc(network, "drainage_X10321", "drainage_X10302")
 
+# fitting bayesian network (parameters are estimated via maximum likelihood (method = "mle-g"))
 fitted_network <- bn.fit(network, data = data_merged_rol[, variables_needed])
+# model fit?
+bn.fit.qqplot(fitted_network)
+bn.fit.xyplot(fitted_network)
+bn.fit.histogram(fitted_network)
 
 # visualization
 plot_obj <- graphviz.plot(fitted_network)
 plot(plot_obj)
+
+
+
+
+# examine normal distribution assumption
+i <- 38
+ggplot(data_merged_rol, aes(x = get(variables_needed[i]))) +
+  geom_density() +
+  labs(x = variables_needed[i])
+
+
+
+
+# information criteria?
+BIC(fitted_network, data = na.omit(data_merged_rol[, variables_needed]))
+AIC(fitted_network, data = na.omit(data_merged_rol[, variables_needed]))
+
+
+
+# measuring point X10302
+predicted_values_X10302 <- predict(fitted_network, node = "drainage_X10302", data = data_merged_rol[, variables_needed], method = "bayes-lw")
+plot(data_merged_rol$drainage_X10302, predicted_values_X10302, xlim = c(0, 800), ylim = c(0, 800))
+# rather overestimates values
+
+fitted_network$drainage_X10302
+residuals_X10302 <- data_merged_rol$drainage_X10302 - predicted_values_X10302
+boxplot(residuals_X10302)
+plot(data_merged_rol$drainage_X10302, residuals_X10302)
+
+
+# measuring point X10303
+predicted_values_X10303 <- predict(fitted_network, node = "drainage_X10303", data = data_merged_rol[, variables_needed], method = "bayes-lw")
+plot(data_merged_rol$drainage_X10303, predicted_values_X10303, xlim = c(0, 350), ylim = c(0, 350))
+# rather underestimates values
+
+fitted_network$drainage_X10303
+residuals_X10303 <- data_merged_rol$drainage_X10303 - predicted_values_X10303
+boxplot(residuals_X10303)
+plot(data_merged_rol$drainage_X10303, residuals_X10303)
+
+
+# measuring point X10304
+predicted_values_X10304 <- predict(fitted_network, node = "drainage_X10304", data = data_merged_rol[, variables_needed], method = "bayes-lw")
+plot(data_merged_rol$drainage_X10304, predicted_values_X10304, xlim = c(0, 130), ylim = c(0, 130))
+# rather underestimates values
+
+fitted_network$drainage_X10304
+residuals_X10304 <- data_merged_rol$drainage_X10304 - predicted_values_X10304
+boxplot(residuals_X10304)
+plot(data_merged_rol$drainage_X10304, residuals_X10304)
+
+
+# measuring point X10321
+predicted_values_X10321 <- predict(fitted_network, node = "drainage_X10321", data = data_merged_rol[, variables_needed], method = "bayes-lw")
+plot(data_merged_rol$drainage_X10321, predicted_values_X10321, xlim = c(0, 300), ylim = c(0, 300))
+# rather underestimates values
+
+fitted_network$drainage_X10321
+residuals_X10321 <- data_merged_rol$drainage_X10321 - predicted_values_X10321
+boxplot(residuals_X10321)
+plot(data_merged_rol$drainage_X10321, residuals_X10321)
